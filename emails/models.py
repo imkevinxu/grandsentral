407cda1f9228f30b2f4cc574c7b25df94a167256
Kevin Xu <kevin@imkevinxu.com>
2012-03-04 19:25:26 -0800

    bugfix

--- emails/models.py
+++ emails/models.py
@@ -3,9 +3,9 @@ from django.contrib import admin
 
 class Email(models.Model):
 	from_name 		= models.CharField(max_length=50)
-	from_email		= models.CharField(max_length=50)
+	from_email		= models.EmailField()
 	to_name   		= models.CharField(max_length=50)
-	to_email  		= models.CharField(max_length=50)
+	to_email  		= models.EmailField()
 	subject   		= models.CharField(max_length=100)
 	body      		= models.TextField()
 
